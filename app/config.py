b3c40cde22a116e13dad3a81b0123c4821dae049
Henry-0810 <ml2002stduy@gmail.com>
2025-04-27 18:53:15 +0100

    done

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Settings:
-    KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
+    KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
     KAFKA_RAW_TOPIC = os.getenv("KAFKA_RAW_TOPIC", "card-raw-data-topic")
     KAFKA_PREPROCESSED_TOPIC = os.getenv(
         "KAFKA_PROCESSED_TOPIC", "card-processed-data-topic"
